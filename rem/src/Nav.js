bd5069308f14cd74a132e33f725870d5bd784d57
expnoob <sudeep19313@iiitd.ac.in>
2021-12-11 12:51:31 +0530

    Updated project to react and firebase
    Added firebase email auth and react components & routes

---
+++ rem/src/Nav.js
@@ -0,0 +1,18 @@
+import './css/style.css';
+
+function Nav() {
+
+  return (
+    <div className = "navbar">
+    <button className = "nvButton">
+        Home
+    </button>
+    <button className = "nvButton">
+        About
+    </button>
+
+</div>
+  );
+}
+
+export default Nav;
---
+++ rem/src/buyerPage.js
@@ -0,0 +1,14 @@
+import './css/style.css';
+import {useNavigate} from "react-router-dom";
+function _sellerDashboard() {
+ let navigate = useNavigate()
+  return (
+    <div>
+        buyer DB
+    </div>
+    
+
+  );
+}
+
+export default _sellerDashboard;
